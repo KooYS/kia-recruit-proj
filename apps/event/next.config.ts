1b1d2f61ef4441d34a2e620051afb45e0a811a3c
KooYS <tlfkthsl42@gmail.com>
2025-03-19 16:14:37 +0900

    chore: prisma bug 수정 vercel에서 파일 삭제 하는 이슈

--- apps/event/next.config.ts
+++ apps/event/next.config.ts
@@ -1,14 +1,23 @@
-import path from "path";
-import { fileURLToPath } from "url";
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
-  transpilePackages: ["@repo/ui"],
-  outputFileTracingRoot: path.join(__dirname, "../../"),
+  output: 'standalone',
+  transpilePackages: ['@repo/ui'],
+  outputFileTracingRoot: path.join(__dirname, '../../'),
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
